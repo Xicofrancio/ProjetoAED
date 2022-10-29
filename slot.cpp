7c2dd193a404880eaaec0c05db3cf09a6cf65702
Xicofrancio <franciscodpfsousa@gmail.com>
2022-10-29 14:52:39 +0100

    Inicializaçao de funcoes e construtores da classe slot

---
+++ slot.cpp
@@ -0,0 +1,49 @@
+//
+// Created by USER on 29/10/2022.
+//
+
+#include "estudante.h"
+#include "gestaohorarios.h"
+#include "pedido.h"
+#include "slot.h"
+#include "turmah.h"
+#include "ucturma.h"
+
+Slot::Slot(std::string *Dia, float HoraInicio, float HoraFim, std::string *Tipo) {
+    dia = *Dia;
+    horaInicio = HoraInicio;
+    horaFim = HoraFim;
+    tipo = *Tipo;
+}
+
+std::string Slot::getDia() {
+    return dia;
+}
+
+float Slot::getHoraInicio() {
+    return horaInicio;
+}
+
+float Slot::getHoraFim() {
+    return horaFim;
+}
+
+std::string Slot::getTipo() {
+    return tipo;
+}
+
+void Slot::setDia(std::string *Dia) {
+    dia = *Dia;
+}
+
+void Slot::setHoraInicio(float HoraInicio) {
+    horaInicio = HoraInicio;
+}
+
+void Slot::setHoraFim(float HoraFim) {
+    horaFim = HoraFim;
+}
+
+void Slot::setTipo(std::string *Tipo) {
+    tipo = *Tipo;
+}
--- slot.h
+++ slot.h
@@ -12,15 +12,15 @@ private:
     float horaFim;
     std::string tipo;
 public:
-    std::string getDia();
-    void setDia();
+    std::string getDia() ;
+    void setDia(std::string *Dia);
     float getHoraInicio();
-    void setHoraInicio();
+    void setHoraInicio(float HoraInicio);
     float getHoraFim();
-    void setHoraFim();
+    void setHoraFim(float HoraFim);
     std::string getTipo();
-    void setTipo();
-    Slot(std::string Dia, float HoraInicio, float HoraFim, std::string Tipo);
+    void setTipo(std::string *Tipo);
+    Slot(std::string *Dia, float HoraInicio, float HoraFim, std::string *Tipo);
 };
 
 #endif //SLOT_H
